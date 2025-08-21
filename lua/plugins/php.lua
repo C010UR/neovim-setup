f8da06a059910a43ad2c842c59685d0d4dcfd2fb
Mikhail Buinouski <m.buinovski@richbrains.net>
2025-08-21 16:11:50 +0300

    Update configuration

---
+++ lua/plugins/php.lua
@@ -0,0 +1,85 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "php" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          enabled = true,
+          settings = {
+            intelephense = {
+              maxMemory = 8192,
+              files = {
+                maxSize = 10000000,
+              },
+              environment = {
+                phpVersion = "8.4",
+              },
+              format = {
+                enable = true,
+                braces = "psr12",
+              },
+            },
+          },
+          init_options = {
+            storagePath = vim.fn.expand("~/.cache/intelephense-local"),
+            globalStoragePath = vim.fn.expand("~/.cache/intelephense-global"),
+          },
+        },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "phpcs",
+        "php-cs-fixer",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      local path = require("mason-registry").get_package("php-debug-adapter"):get_install_path()
+      dap.adapters.php = {
+        type = "executable",
+        command = "node",
+        args = { path .. "/extension/out/phpDebug.js" },
+      }
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.phpcsfixer)
+      table.insert(opts.sources, nls.builtins.diagnostics.phpcs)
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = { "phpcs" },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "php_cs_fixer" },
+      },
+    },
+  },
+}
